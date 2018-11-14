f3e39f5a5fdbf7ac66d69fb0f52e07171bcb94c7
Samentha Shelley <samenthashelley@Samenthas-MacBook-Pro.local>
2018-11-13 19:25:50 -0800

    first commit

---
+++ src/Application.java
@@ -0,0 +1,46 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double numd = 120.00d;
+		double numf = 10d;
+		char achar = 'h';
+		long along = 123222L;
+		boolean isHot = false;
+		
+		short ashort =123;
+		int aint = -123;
+		float pointX = 12.0f;
+		
+		String name = "kris";
+		byte abyte = 0; 
+		//= assignment of value
+		
+		//+ add value
+		double sum = numd + numf;
+		double sum2 = 10 +7;
+		
+		// * multiplication gets product
+		double product = 100 * 7;
+		
+		// ( / ) division gets quotients
+		float quoatient = 10.1f / 2f; 
+		//% modulous get the remainder of the quotient
+		
+		int remainder = 10 % 2; //==5
+		
+		int number = 1;
+		number ++;
+		number = number + 1;
+		
+		//compare
+		//1 == 2 this would be false 
+		// 1 != 2 This would be true this is saying ! is not equal to
+		
+		System.out.println(name);
+		System.out.println(numd + 5);
+		
+	}
+
+}
